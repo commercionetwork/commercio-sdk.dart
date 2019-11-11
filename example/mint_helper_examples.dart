faf89a90e8609cc4303098721fcb7e0cba6fdd77
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-11-11 10:08:12 +0100

    Added MintHelper examples

---
+++ example/mint_helper_examples.dart
@@ -0,0 +1,63 @@
+import 'package:commerciosdk/export.dart';
+
+import 'commons.dart';
+
+void main() async {
+  final info = NetworkInfo(
+    bech32Hrp: "did:com:",
+    lcdUrl: "http://localhost:1317",
+  );
+
+  final userMnemonic = [
+    "will",
+    "hard",
+    "topic",
+    "spray",
+    "beyond",
+    "ostrich",
+    "moral",
+    "morning",
+    "gas",
+    "loyal",
+    "couch",
+    "horn",
+    "boss",
+    "across",
+    "age",
+    "post",
+    "october",
+    "blur",
+    "piece",
+    "wheel",
+    "film",
+    "notable",
+    "word",
+    "man"
+  ];
+
+  final userWallet = Wallet.derive(userMnemonic, info);
+
+  // --- Open CDP
+  // _openCdp(amount: 100000, wallet: userWallet);
+
+  // --- Close CDP
+  // _closeCdp(timestamp: 757, wallet: userWallet);
+}
+
+/// Shows how to open a new Collateralized Debt Position in order to get half
+/// the specified [amount] of Commercio Cash Credits millionth parts (`uccc`).
+/// Please note that `uccc` are millionth of Commercio Cash Credits and thus to
+/// send one document you wil need 10.000 `uccc`.
+Future<void> _openCdp({int amount, Wallet wallet}) async {
+  final response = await MintHelper.openCdp(amount, wallet);
+  checkResponse(response);
+}
+
+/// Shows how to close a Collateralized Debt Position in order to allow the
+/// user controlling the given [wallet] to get back the amount of pico
+/// Commercio Tokens (`ucommercio`) giving back the lent pico Commercio
+/// Cash Credits (`uccc`).
+Future<void> _closeCdp({int timestamp, Wallet wallet}) async {
+  final response = await MintHelper.closeCdp(timestamp, wallet);
+  checkResponse(response);
+}
--- lib/mint/mint_helper.dart
+++ lib/mint/mint_helper.dart
@@ -3,29 +3,22 @@ import 'package:sacco/sacco.dart';
 
 /// Allows to easily perform CommercioMINT related transactions.
 class MintHelper {
-  /// Opens a new CDP depositing the given [commercioTokenAmount].
-  static Future<TransactionResult> openCdp(
-    int commercioTokenAmount,
-    Wallet wallet,
-  ) {
+  /// Opens a new CDP depositing the given Commercio Token [amount].
+  static Future<TransactionResult> openCdp(int amount, Wallet wallet) {
     final msg = MsgOpenCdp(
       depositAmount: [
         StdCoin(
           denom: "ucommercio",
-          amount: (commercioTokenAmount * 1000000).toString(),
+          amount: (amount * 1000000).toString(),
         )
       ],
       signerDid: wallet.bech32Address,
-      timeStamp: getTimeStamp(),
     );
     return TxHelper.createSignAndSendTx([msg], wallet);
   }
 
   /// Closes the CDP having the given [timestamp].
-  static Future<TransactionResult> closeCdp(
-    String timestamp,
-    Wallet wallet,
-  ) {
+  static Future<TransactionResult> closeCdp(int timestamp, Wallet wallet) {
     final msg = MsgCloseCdp(
       timeStamp: timestamp,
       signerDid: wallet.bech32Address,
