d990bbf4a73d4d8f3c8c9ccf0ab9675a13f48193
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-04-06 11:56:41 +0200

    Add optional fee parameter with implicit conversion of the amount

--- lib/mint/mint_helper.dart
+++ lib/mint/mint_helper.dart
@@ -4,25 +4,27 @@ import 'package:sacco/sacco.dart';
 /// Allows to easily perform CommercioMINT related transactions.
 class MintHelper {
   /// Opens a new CDP depositing the given Commercio Token [amount].
-  static Future<TransactionResult> openCdp(int amount, Wallet wallet) {
+  static Future<TransactionResult> openCdp(int amount, Wallet wallet,
+      {StdFee fee}) {
+    final depositAmount = fee.amount
+        .map((stdCoin) => StdCoin(
+            denom: stdCoin.denom,
+            amount: (stdCoin.amount * 1000000).toString()))
+        .toList();
     final msg = MsgOpenCdp(
-      depositAmount: [
-        StdCoin(
-          denom: "ucommercio",
-          amount: (amount * 1000000).toString(),
-        )
-      ],
+      depositAmount: depositAmount,
       signerDid: wallet.bech32Address,
     );
     return TxHelper.createSignAndSendTx([msg], wallet);
   }
 
   /// Closes the CDP having the given [timestamp].
-  static Future<TransactionResult> closeCdp(int timestamp, Wallet wallet) {
+  static Future<TransactionResult> closeCdp(int timestamp, Wallet wallet,
+      {StdFee fee}) {
     final msg = MsgCloseCdp(
       timeStamp: timestamp,
       signerDid: wallet.bech32Address,
     );
-    return TxHelper.createSignAndSendTx([msg], wallet);
+    return TxHelper.createSignAndSendTx([msg], wallet, fee: fee);
   }
 }
