358c2d662b9b8ed2f2507dfc69b4eef552c77b22
fulvio.zanetti <fulvio.zanetti@gmail.com>
2020-04-27 13:49:37 +0200

    Fixed export missing class

--- lib/id/export.dart
+++ lib/id/export.dart
@@ -1,5 +1,3 @@
-export 'did_deposit_request_payload.dart';
-export 'did_deposit_request_signature_json.dart';
 export 'did_document_helper.dart';
 export 'did_document_proof_signature_content.dart';
 export 'did_power_up_request_payload.dart';
