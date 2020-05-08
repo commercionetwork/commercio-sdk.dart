cbf0ee8803a795cc6a706cdb451a730b28eb8399
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-05-08 18:07:03 +0200

    Add sender_did field

--- lib/id/did_power_up_request_payload.g.dart
+++ lib/id/did_power_up_request_payload.g.dart
@@ -9,6 +9,7 @@ part of 'did_power_up_request_payload.dart';
 DidPowerUpRequestPayload _$DidPowerUpRequestPayloadFromJson(
     Map<String, dynamic> json) {
   return DidPowerUpRequestPayload(
+    senderDid: json['sender_did'] as String,
     pairwiseDid: json['pairwise_did'] as String,
     timestamp: json['timestamp'] as String,
     signature: json['signature'] as String,
@@ -18,6 +19,7 @@ DidPowerUpRequestPayload _$DidPowerUpRequestPayloadFromJson(
 Map<String, dynamic> _$DidPowerUpRequestPayloadToJson(
         DidPowerUpRequestPayload instance) =>
     <String, dynamic>{
+      'sender_did': instance.senderDid,
       'pairwise_did': instance.pairwiseDid,
       'timestamp': instance.timestamp,
       'signature': instance.signature,
