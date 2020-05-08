666e1889645dab98e40a22354c853bf66a3153e9
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-05-08 18:05:58 +0200

    Add signPowerUpSignature to create the signature field

--- lib/crypto/sign_helper.dart
+++ lib/crypto/sign_helper.dart
@@ -1,6 +1,10 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:commerciosdk/entities/export.dart';
+import 'package:commerciosdk/id/export.dart';
+import 'package:crypto/crypto.dart';
+import 'package:meta/meta.dart';
 import 'package:sacco/sacco.dart';
 import 'package:sacco/utils/export.dart';
 
@@ -15,5 +19,20 @@ class SignHelper {
     }
 
     return wallet.sign(utf8.encode(json.encode(sorted)));
+  }
+
+  /// Takes the [signatureJson] and:
+  /// 1. Concatenate senderDid, pairwiseDid and timestamp as payload
+  /// 2. Calcolate the SHA256 hash of the payload
+  /// 3. Returns the RSA PKCS1v15 encription of the hash using wallet's 
+  ///    private key 
+  static Uint8List signPowerUpSignature(
+      {@required DidPowerUpRequestSignatureJson signatureJson,
+      @required Wallet wallet}) {
+    final concat = signatureJson.senderDid +
+        signatureJson.pairwiseDid +
+        signatureJson.timestamp;
+    final hash = sha256.convert(utf8.encode(concat));
+    return wallet.sign(hash.bytes);
   }
 }
