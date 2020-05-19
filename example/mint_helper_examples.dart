0ccd57870cd501bb662e40dde4cf1883ccefe590
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-05-19 11:07:52 +0200

    Update examples to meet new sdk 2.1.0 specifications

--- example/mint_helper_examples.dart
+++ example/mint_helper_examples.dart
@@ -2,54 +2,57 @@ import 'package:commerciosdk/export.dart';
 
 import 'commons.dart';
 
+final userMnemonic = [
+  'will',
+  'hard',
+  'topic',
+  'spray',
+  'beyond',
+  'ostrich',
+  'moral',
+  'morning',
+  'gas',
+  'loyal',
+  'couch',
+  'horn',
+  'boss',
+  'across',
+  'age',
+  'post',
+  'october',
+  'blur',
+  'piece',
+  'wheel',
+  'film',
+  'notable',
+  'word',
+  'man'
+];
+
 void main() async {
-  final info = NetworkInfo(
-    bech32Hrp: "did:com:",
-    lcdUrl: "http://localhost:1317",
+  final networkInfo = NetworkInfo(
+    bech32Hrp: 'did:com:',
+    lcdUrl: 'http://localhost:1317',
   );
 
-  final userMnemonic = [
-    "will",
-    "hard",
-    "topic",
-    "spray",
-    "beyond",
-    "ostrich",
-    "moral",
-    "morning",
-    "gas",
-    "loyal",
-    "couch",
-    "horn",
-    "boss",
-    "across",
-    "age",
-    "post",
-    "october",
-    "blur",
-    "piece",
-    "wheel",
-    "film",
-    "notable",
-    "word",
-    "man"
-  ];
-
-  final userWallet = Wallet.derive(userMnemonic, info);
+  final userWallet = Wallet.derive(userMnemonic, networkInfo);
 
   // --- Open CDP
-  // await _openCdp(amount: 100000, wallet: userWallet);
+  await _openCdp(amount: 100000, wallet: userWallet);
 
   // --- Close CDP
-  // await _closeCdp(timestamp: 757, wallet: userWallet);
+  await _closeCdp(timestamp: 757, wallet: userWallet);
 }
 
 /// Shows how to open a new Collateralized Debt Position in order to get half
 /// the specified [amount] of Commercio Cash Credits millionth parts (`uccc`).
 /// Please note that `uccc` are millionth of Commercio Cash Credits and thus to
 /// send one document you wil need 10.000 `uccc`.
+///
+/// Docs: https://docs.commercio.network/x/commerciomint/#open-a-cdp
 Future<void> _openCdp({int amount, Wallet wallet}) async {
   final response = await MintHelper.openCdp(amount, wallet);
+
   checkResponse(response);
 }
 
@@ -57,7 +60,12 @@ Future<void> _openCdp({int amount, Wallet wallet}) async {
 /// user controlling the given [wallet] to get back the amount of pico
 /// Commercio Tokens (`ucommercio`) giving back the lent pico Commercio
 /// Cash Credits (`uccc`).
+/// The [timestamp] is the block height at which the CDP is was inserted into
+///  the chain.
+///
+/// Docs: https://docs.commercio.network/x/commerciomint/#close-a-cdp
 Future<void> _closeCdp({int timestamp, Wallet wallet}) async {
   final response = await MintHelper.closeCdp(timestamp, wallet);
+
   checkResponse(response);
 }
