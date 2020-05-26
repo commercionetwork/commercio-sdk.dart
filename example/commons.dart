48254a0a33164309e2db8bf87eee5fbd09cc9de6
zenit70 <fulvio.zanetti@gmail.com>
2020-05-26 09:59:48 +0200

    Removed example folder

--- example/commons.dart
+++
@@ -1,9 +0,0 @@
-import 'package:commerciosdk/export.dart';
-
-void checkResponse(TransactionResult response) {
-  if (response.success) {
-    print(response.hash);
-  } else {
-    print(response.error.errorMessage);
-  }
-}
