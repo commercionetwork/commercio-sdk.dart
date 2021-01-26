c1c544395b3d7e541e0a9d21558d0a6a26825a50
Enrico Pasquali <pasquali.enrico94@gmail.com>
2021-01-26 11:45:42 +0100

    Add matchBech32Format(), update docs & style

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -44,19 +44,62 @@ StdFee calculateDefaultFee({
   );
 }
 
-/// Checks if the bytes length of a string [str]
-/// is less than a certain value [len]
-bool checkStringBytesLen(String str, int len) {
-  List<int> bytes = utf8.encode(str);
-  return bytes.length <= len;
+/// Returns the number of bytes of a UTF-8 encoded [str]. If [str] is null then
+/// 0 is returned.
+int getStringBytes(String str) {
+  if (str == null) {
+    return null;
+  }
+
+  final bytes = utf8.encode(str);
+  return bytes.length;
 }
 
-/// Checks if a string [uuid] has a Uuid-v4 format
+/// Return [true] if the string [uuid] has a Uuid-v4 format.
 bool matchUuidv4(String uuid) {
-  RegExp regExp = new RegExp(
-    r"^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$",
+  final regExp = RegExp(
+    r'^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$',
     caseSensitive: false,
-    multiLine: false,
   );
   return regExp.hasMatch(uuid);
+}
+
+/// Returns [true] if the provided [str] is a valid bech32 string.
+/// See: https://en.bitcoin.it/wiki/Bech32
+bool matchBech32Format(String str) {
+  if (str == null) {
+    throw ArgumentError('(str) must not be null');
+  }
+
+  if (str.length < 8 || str.length > 90) {
+    return false;
+  }
+
+  final regExp = RegExp(
+    r'^(\S{1,83})(1)([^1bio]{6,88})$',
+    caseSensitive: false,
+  );
+
+  final match = regExp.hasMatch(str);
+
+  return match;
+}
+
+/// TODO(pasqenr): Maybe we should support Bech32 checksum validation.
+/// See: https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki#Bech32
+// ignore: unused_element
+int _bech32Polymod(String str) {
+  const GEN = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
+  var chk = 1;
+
+  for (final v in str.codeUnits) {
+    final b = (chk >> 25);
+    chk = (chk & 0x1ffffff) << 5 ^ v;
+
+    for (var i = 0; i < 5; i++) {
+      chk ^= ((b >> i) & 1) == 1 ? GEN[i] : 0;
+    }
+  }
+
+  return chk;
 }
--- test/utils/utils_test.dart
+++ test/utils/utils_test.dart
@@ -2,10 +2,14 @@ import 'package:commerciosdk/export.dart';
 import 'package:test/test.dart';
 
 void main() {
+  const str100CharsLen =
+      'xRqpfSGKVF8Qor1HcKhy2gwmUw0fSZ25IXp7y2DdJctxTyqsiP4YruHIWjY2Qkhe8aZxag9Smi3NwMrFs5CPYzkUF3P4sCOYmlRI';
+
   group('Function "calculateDefaultFee" returns expected values;', () {
     const defaultAmount = 100;
     const defaultDenom = "commercio";
     const defaultGas = 200;
+
     test('if the messages number is greater than 1, the fees are multiplied.',
         () {
       final msgsNumber = 2;
@@ -28,6 +32,64 @@ void main() {
       );
 
       expect(fee.toJson(), expectedFee.toJson());
+    });
+  });
+
+  group('matchUuidv4', () {
+    test('Null value should throw ArgumentError', () {
+      expect(() => matchUuidv4(null), throwsArgumentError);
+    });
+
+    test('Invalid strings should return false', () {
+      // Empty
+      expect(matchUuidv4(''), isFalse);
+
+      // UUID v1
+      expect(matchUuidv4('690f3bf4-5fbf-11eb-ae93-0242ac130002'), isFalse);
+
+      // UUID v3, v5
+      expect(matchUuidv4('6ba7b810-9dad-11d1-80b4-00c04fd430c8'), isFalse);
+
+      // UUID null
+      expect(matchUuidv4('00000000-0000-0000-0000-000000000000'), isFalse);
+    });
+
+    test('Valid UUID v4 should return true', () {
+      expect(matchUuidv4('e21fd7bf-fd58-40ca-be2d-f50d29189276'), isTrue);
+    });
+  });
+
+  group('matchBech32Format', () {
+    test('Null value should throw ArgumentError', () {
+      expect(() => matchBech32Format(null), throwsArgumentError);
+    });
+
+    test('String lenght not between 8 and 90 should return false', () {
+      expect(matchBech32Format(''), isFalse);
+      expect(matchBech32Format(str100CharsLen), isFalse);
+    });
+
+    test('Bech32 with "b", "i" or "o" after the 1 should return false', () {
+      expect(matchBech32Format('did:com:1b'), isFalse);
+      expect(matchBech32Format('did:com:1i'), isFalse);
+      expect(matchBech32Format('did:com:1o'), isFalse);
+    });
+
+    test('Bech32 without the "1" as separator should return false', () {
+      expect(matchBech32Format('did:com:acdefg'), isFalse);
+    });
+
+    test('Bech32 without the hrp part before "1" should return false', () {
+      expect(matchBech32Format('1acdefg'), isFalse);
+    });
+
+    test('Valid Bech32 should return true', () {
+      expect(matchBech32Format('did:com:1acdefg'), isTrue);
+      expect(
+        matchBech32Format('did:com:1mfddzjvr8vpeqdtm6fuay2nvnecuk9qa8usqq5'),
+        isTrue,
+      );
+      expect(matchBech32Format('cosmos1acdefg'), isTrue);
     });
   });
 }
