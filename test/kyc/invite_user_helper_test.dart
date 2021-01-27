afc7a525ec4fbe7766c8dbf65f5797c2d35d5282
zenit70 <fulvio.zanetti@gmail.com>
2021-01-27 16:00:39 +0100

    Implemented tests

--- test/kyc/invite_user_helper_test.dart
+++ test/kyc/invite_user_helper_test.dart
@@ -8,13 +8,19 @@ void main() {
         "dash ordinary anxiety zone slot rail flavor tortoise guilt divert pet sound ostrich increase resist short ship lift town ice split payment round apology";
     final mnemonic = mnemonicString.split(" ");
     final wallet = Wallet.derive(mnemonic, networkInfo);
-    final recipientDid = 'did:com:id';
 
-    test('"fromWallet()" returns a well-formed "InviteUser" object.', () {
+    test('if "fromWallet()" returns a well-formed "InviteUser" object.', () {
+      final recipientDid = 'did:com:id';
+
       final expectedInviteUser = InviteUser(
-          recipientDid: recipientDid, senderDid: wallet.bech32Address);
+        recipientDid: recipientDid,
+        senderDid: wallet.bech32Address,
+      );
 
-      final inviteUser = InviteUserHelper.fromWallet(wallet, recipientDid);
+      final inviteUser = InviteUserHelper.fromWallet(
+        wallet: wallet,
+        recipientDid: recipientDid,
+      );
 
       expect(inviteUser.toJson(), expectedInviteUser.toJson());
     });
