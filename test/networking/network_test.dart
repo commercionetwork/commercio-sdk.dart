92f74d82b8e438d899d3d02366f87aaa7edc0c53
Enrico Pasquali <pasquali.enrico94@gmail.com>
2021-01-28 13:41:02 +0100

    Complete tests for network helper

--- test/networking/network_test.dart
+++ test/networking/network_test.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:commerciosdk/export.dart';
@@ -26,6 +27,54 @@ void main() {
     expect('did:com:1zfhgwfgex8rc9t00pk6jm6xj6vx5cjr4ngy32v',
         testDataList[0].sender);
     expect('6a881ef0-04da-4524-b7ca-6e5e3b7e61dc', testDataList[1].uuid);
+  });
+
+  test('query with response status != 200 should return null', () async {
+    // Build the mock client
+    final clientMock = MockClient((request) async {
+      return Response('', 400);
+    });
+
+    final result = await Network.query(
+      'http://example.com',
+      client: clientMock,
+    );
+
+    expect(result, isNull);
+  });
+
+  test(
+      'queryChain should return null if the returned JSON does not have the key "result"',
+      () async {
+    // Build the mock client
+    final clientMock = MockClient((request) async {
+      return Response(jsonEncode({}), 200);
+    });
+
+    final result = await Network.queryChain(
+      'http://example.com',
+      client: clientMock,
+    );
+
+    expect(result, isNull);
+  });
+
+  test('queryChain without json in body should return null', () async {
+    // Build the mock client
+    final clientMock = MockClient((request) async {
+      return Response('', 200);
+    });
+
+    final result =
+        await Network.queryChain('http://example.com', client: clientMock);
+
+    expect(result, isNull);
+  });
+
+  test('Default client should return null in test', () async {
+    final response = await Network.query('');
+
+    expect(response, isNull);
   });
 }
 
