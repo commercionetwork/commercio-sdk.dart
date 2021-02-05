6783b79f18d88dbdbc571255e895bbcee83ad2d1
zenit70 <fulvio.zanetti@gmail.com>
2021-02-05 13:53:42 +0100

    Fixed module export

--- lib/export.dart
+++ lib/export.dart
@@ -5,7 +5,7 @@ export 'docs/export.dart';
 export 'entities/export.dart' hide RSAKeyParser;
 export 'id/export.dart';
 export 'membership/export.dart';
-export 'mint/mint_helper.dart';
+export 'mint/export.dart';
 export 'networking/network.dart';
 export 'tx/tx_helper.dart';
 export 'utils/utils.dart';
