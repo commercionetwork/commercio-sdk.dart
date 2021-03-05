34c247cde6651134809105b1215e7f346a727d13
Enrico Pasquali <pasquali.enrico94@gmail.com>
2021-03-05 15:34:31 +0100

    Parse PEM RSA files using package rsa_pkcs

--- lib/crypto/rsa_key_parser.dart
+++ lib/crypto/rsa_key_parser.dart
@@ -1,12 +1,30 @@
-import 'package:encrypt/encrypt.dart' as encrypt;
-import 'package:pointycastle/export.dart' as pointy_castle;
+import 'package:pointycastle/export.dart';
+import 'package:rsa_pkcs/rsa_pkcs.dart' as rsa_parser;
 
-/// Allows to easily parse a PEM-encoded RSA public key.
+/// RSA PEM parser.
 class RSAKeyParser {
-  /// Reads the given [pemString] as a PEM-encoded RSA key,
-  /// returning both public and private keys.
-  static pointy_castle.RSAAsymmetricKey parseKeyFromPem(String pemString) {
-    final parser = encrypt.RSAKeyParser();
-    return parser.parse(pemString);
+  /// Parses the PEM key no matter it is public or private, it will figure it out.
+  static RSAAsymmetricKey parseFromPem(String key) {
+    final parser = rsa_parser.RSAPKCSParser();
+
+    final pair = parser.parsePEM(key);
+
+    if (pair.private != null) {
+      final k = pair.private;
+
+      return RSAPrivateKey(
+        k.modulus,
+        k.privateExponent,
+        k.prime1,
+        k.prime2,
+        BigInt.from(k.publicExponent),
+      );
+    } else if (pair.public != null) {
+      final k = pair.public;
+
+      return RSAPublicKey(k.modulus, BigInt.from(k.publicExponent));
+    }
+
+    throw ArgumentError('Could only parse PCKS1 RSA PEM files');
   }
 }
