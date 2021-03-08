7e4ab31fa40e97d5257b84570d70588daa3eb056
Enrico Pasquali <pasquali.enrico94@gmail.com>
2021-03-08 16:58:28 +0100

    Use internal RSA PEM/DER parser

--- lib/crypto/rsa_key_parser.dart
+++ lib/crypto/rsa_key_parser.dart
@@ -1,30 +1,188 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:asn1lib/asn1lib.dart';
 import 'package:pointycastle/export.dart';
-import 'package:rsa_pkcs/rsa_pkcs.dart' as rsa_parser;
 
 /// RSA PEM parser.
 class RSAKeyParser {
+  static const String pkcs1PublicHeader = '-----BEGIN RSA PUBLIC KEY-----';
+  static const String certPublicKeyHeader = '-----BEGIN PUBLIC KEY-----';
+  static const String pkcs1PublicFooter = '-----END RSA PUBLIC KEY-----';
+  static const String certPublicKeyFooter = '-----END PUBLIC KEY-----';
+
+  static const String pkcs1PrivateHeader = '-----BEGIN RSA PRIVATE KEY-----';
+  static const String pkcs8PrivateHeader = '-----BEGIN PRIVATE KEY-----';
+  static const String pkcs1PrivateFooter = '-----END RSA PRIVATE KEY-----';
+  static const String pkcs8PrivateFooter = '-----END PRIVATE KEY-----';
+
+  static const String pkcsStartHeader = '-----BEGIN ';
+  static const String pkcsStartFooter = '-----END';
+
   /// Parses the PEM key no matter it is public or private, it will figure it out.
-  static RSAAsymmetricKey parseFromPem(String key) {
-    final parser = rsa_parser.RSAPKCSParser();
+  static RSAAsymmetricKey parseFromPem(String pem) {
+    final lines = pem
+        .split('\n')
+        .map((String line) => line.trim())
+        .where((String line) => line.isNotEmpty)
+        .toList();
 
-    final pair = parser.parsePEM(key);
+    final headerIndex = lines.indexWhere((s) => s.contains(pkcsStartHeader));
+    final footerIndex = lines.indexWhere((s) => s.contains(pkcsStartFooter));
 
-    if (pair.private != null) {
-      final k = pair.private;
+    final body = lines.sublist(headerIndex + 1, footerIndex).join('');
+    final bodyBytes = base64Decode(body);
 
-      return RSAPrivateKey(
-        k.modulus,
-        k.privateExponent,
-        k.prime1,
-        k.prime2,
-        BigInt.from(k.publicExponent),
-      );
-    } else if (pair.public != null) {
-      final k = pair.public;
-
-      return RSAPublicKey(k.modulus, BigInt.from(k.publicExponent));
+    if (lines[0].contains(pkcs1PublicHeader)) {
+      return decodePKCS1PublicKey(bodyBytes);
+    } else if (lines[0].contains(pkcs1PrivateHeader)) {
+      return decodePKCS1PrivateKey(bodyBytes);
+    } else if (lines[0].contains(pkcs8PrivateHeader)) {
+      return decodePKCS8PrivateKey(bodyBytes);
+    } else if (lines[0].contains(certPublicKeyHeader)) {
+      return decodeCertPublicKey(bodyBytes) as RSAPublicKey;
     }
 
-    throw ArgumentError('Could only parse PCKS1 RSA PEM files');
+    throw ArgumentError('Could only parse PCKS1/PKCS8 RSA PEM files');
+  }
+
+  /// Format:
+  /// ```
+  /// RSAPublicKey ::= SEQUENCE {
+  ///     modulus           INTEGER,  -- n
+  ///     publicExponent    INTEGER   -- e
+  /// }
+  /// ```
+  ///
+  ///
+  /// See: https://tools.ietf.org/html/rfc8017#appendix-A
+  // TODO: In fact this decodes both PKCS Public Key types
+  static RSAPublicKey decodePKCS1PublicKey(Uint8List bytes) {
+    final topLevelParser = ASN1Parser(bytes);
+    final topLevelBitStr = topLevelParser.nextObject();
+
+    Uint8List pubKeyBytes;
+    if (topLevelBitStr is ASN1BitString) {
+      final pubKeySequence =
+          ASN1Sequence.fromBytes(topLevelBitStr.contentBytes());
+      pubKeyBytes = pubKeySequence.encodedBytes;
+    } else {
+      pubKeyBytes = bytes;
+    }
+
+    final parser = ASN1Parser(pubKeyBytes);
+
+    if (!parser.hasNext()) {
+      throw ArgumentError('Not a ASN.1 sequence');
+    }
+
+    final seq = parser.nextObject() as ASN1Sequence;
+
+    if (seq.elements.length != 2) {
+      throw ArgumentError(
+          'RSAPublicKey must have exacly 2 elements in sequence');
+    }
+
+    final modulus = seq.elements[0] as ASN1Integer;
+    final pubExp = seq.elements[1] as ASN1Integer;
+
+    return RSAPublicKey(modulus.valueAsBigInteger, pubExp.valueAsBigInteger);
+  }
+
+  /// Format:
+  /// ```
+  /// RSAPrivateKey ::= SEQUENCE {
+  ///     version           Version,
+  ///     modulus           INTEGER,  -- n
+  ///     publicExponent    INTEGER,  -- e
+  ///     privateExponent   INTEGER,  -- d
+  ///     prime1            INTEGER,  -- p
+  ///     prime2            INTEGER,  -- q
+  ///     exponent1         INTEGER,  -- d mod (p-1)
+  ///     exponent2         INTEGER,  -- d mod (q-1)
+  ///     coefficient       INTEGER,  -- (inverse of q) mod p
+  ///     otherPrimeInfos   OtherPrimeInfos OPTIONAL
+  /// }
+  /// ```
+  ///
+  /// See: https://tools.ietf.org/html/rfc8017#appendix-A
+  static RSAPrivateKey decodePKCS1PrivateKey(Uint8List bytes) {
+    final parser = ASN1Parser(bytes);
+
+    if (!parser.hasNext()) {
+      throw ArgumentError('Not a ASN.1 sequence');
+    }
+
+    final seq = parser.nextObject() as ASN1Sequence;
+
+    final modulus = seq.elements[1] as ASN1Integer;
+    final privExp = seq.elements[3] as ASN1Integer;
+    final p = seq.elements[4] as ASN1Integer;
+    final q = seq.elements[5] as ASN1Integer;
+
+    return RSAPrivateKey(
+      modulus.valueAsBigInteger,
+      privExp.valueAsBigInteger,
+      p.valueAsBigInteger,
+      q.valueAsBigInteger,
+    );
+  }
+
+  /// Plain PKCS8 private key format:
+  /// ```
+  /// PrivateKeyInfo ::= SEQUENCE {
+  ///  version Version,
+  ///  privateKeyAlgorithm AlgorithmIdentifier {{PrivateKeyAlgorithms}},
+  ///  privateKey PrivateKey,
+  ///  attributes [0] Attributes OPTIONAL
+  /// }
+  ///
+  /// Version ::= INTEGER {v1(0)} (v1,...)
+  /// PrivateKey ::= OCTET STRING
+  /// ```
+  static RSAPrivateKey decodePKCS8PrivateKey(Uint8List bytes) {
+    final parser = ASN1Parser(bytes);
+
+    if (!parser.hasNext()) {
+      throw ArgumentError('Not a ASN.1 sequence');
+    }
+
+    // Let's assume that is always a PKCS1 key
+
+    final seq = parser.nextObject() as ASN1Sequence;
+    final privateKey = seq.elements[2] as ASN1OctetString;
+
+    return decodePKCS1PrivateKey(privateKey.octets);
+  }
+
+  /// Format:
+  /// ```
+  /// SubjectPublicKeyInfo  ::=  SEQUENCE  {
+  ///    algorithm            AlgorithmIdentifier,
+  ///    subjectPublicKey     BIT STRING
+  /// }
+  ///
+  /// AlgorithmIdentifier  ::=  SEQUENCE  {
+  ///     algorithm           OBJECT IDENTIFIER,
+  ///     parameters          ANY DEFINED BY algorithm OPTIONAL
+  /// }
+  /// ```
+  static PublicKey decodeCertPublicKey(Uint8List bytes) {
+    final parser = ASN1Parser(bytes);
+
+    if (!parser.hasNext()) {
+      throw ArgumentError('Not a ASN.1 sequence');
+    }
+
+    final pubKeyInfoSeq = parser.nextObject() as ASN1Sequence;
+    final algorithmIdentifier = pubKeyInfoSeq.elements[0] as ASN1Sequence;
+    final algorithm = algorithmIdentifier.elements[0] as ASN1ObjectIdentifier;
+
+    if (algorithm.identifier!.contains('1.2.840.113549.1.1.1')) {
+      final subjectPubKey = pubKeyInfoSeq.elements[1] as ASN1BitString;
+      return decodePKCS1PublicKey(subjectPubKey.encodedBytes);
+    }
+
+    throw ArgumentError('Could only decode PKCS1 RSA Public Key');
   }
 }
--- lib/entities/keys/rsa_keys.dart
+++ lib/entities/keys/rsa_keys.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 
+import 'package:asn1lib/asn1lib.dart';
 import 'package:commerciosdk/entities/export.dart';
 import 'package:pointycastle/export.dart';
-import 'package:asn1lib/asn1lib.dart'; // Do not use pointycastle one
 
 /// Wrapper of the pointyCastle RSAPublicKey
 class CommercioRSAPublicKey implements CommercioPublicKey {
@@ -17,12 +17,22 @@ class CommercioRSAPublicKey implements CommercioPublicKey {
   @override
   String getType() => keyType.value;
 
+  String getPKCS1Encoded() {
+    final pubKeySequence = ASN1Sequence();
+    pubKeySequence.add(ASN1Integer(publicKey.modulus));
+    pubKeySequence.add(ASN1Integer(publicKey.exponent));
+    final dataBase64 = base64Encode(pubKeySequence.encodedBytes);
+
+    return '-----BEGIN RSA PUBLIC KEY-----\n$dataBase64\n-----END RSA PUBLIC KEY-----';
+  }
+
   @override
   String getEncoded() {
     final pubKeySequence = ASN1Sequence();
     pubKeySequence.add(ASN1Integer(publicKey.modulus));
     pubKeySequence.add(ASN1Integer(publicKey.exponent));
     final dataBase64 = base64Encode(pubKeySequence.encodedBytes);
+
     return '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A$dataBase64\n-----END PUBLIC KEY-----';
   }
 }
@@ -33,10 +43,50 @@ class CommercioRSAPrivateKey implements CommercioPrivateKey {
 
   CommercioRSAPrivateKey(this.privateKey);
 
+  /// Plain PKCS8 private key format:
+  /// ```
+  /// PrivateKeyInfo ::= SEQUENCE {
+  ///  version Version,
+  ///  privateKeyAlgorithm AlgorithmIdentifier {{PrivateKeyAlgorithms}},
+  ///  privateKey PrivateKey,
+  ///  attributes [0] Attributes OPTIONAL
+  /// }
+  ///
+  /// Version ::= INTEGER {v1(0)} (v1,...)
+  /// PrivateKey ::= OCTET STRING
+  /// ```
+  String encodePrivateKeyToPlainPemPKCS8() {
+    final pkcs1 = encodePrivateKeyToPemPKCS1();
+    final lines = pkcs1.split('\n');
+    final pkcs1Body = lines.sublist(1, lines.length - 1).join('');
+    final pkcs1BodyBytes = base64Decode(pkcs1Body);
+
+    final algorithmIdentifierSequence = ASN1Sequence()
+      ..add(
+        ASN1ObjectIdentifier('1.2.840.113549.1.1.1'
+            .split('.')
+            .map((e) => int.parse(e))
+            .toList()),
+      );
+
+    final privateKeyInfoSequence = ASN1Sequence();
+    final privateKeyInfoVersion = ASN1Integer.fromInt(0);
+    final privateKeyInfoAlgorithm = algorithmIdentifierSequence;
+    final pKey = ASN1OctetString(pkcs1BodyBytes);
+
+    privateKeyInfoSequence.add(privateKeyInfoVersion);
+    privateKeyInfoSequence.add(privateKeyInfoAlgorithm);
+    privateKeyInfoSequence.add(pKey);
+
+    final pkcs8Body = base64Encode(privateKeyInfoSequence.encodedBytes);
+
+    return '-----BEGIN PRIVATE KEY-----\n$pkcs8Body\n-----END PRIVATE KEY-----';
+  }
+
   String encodePrivateKeyToPemPKCS1() {
     final topLevel = ASN1Sequence();
 
-    final version = ASN1Integer(BigInt.from(0));
+    final version = ASN1Integer.fromInt(0);
     final modulus = ASN1Integer(privateKey.n);
     final publicExponent = ASN1Integer(privateKey.exponent);
     final privateExponent = ASN1Integer(privateKey.privateExponent);
@@ -59,9 +109,9 @@ class CommercioRSAPrivateKey implements CommercioPrivateKey {
     topLevel.add(exp2);
     topLevel.add(co);
 
-    final dataBase64 = base64.encode(topLevel.encodedBytes);
+    final dataBase64 = base64Encode(topLevel.encodedBytes);
 
-    return '-----BEGIN PRIVATE KEY-----\r\n$dataBase64\r\n-----END PRIVATE KEY-----';
+    return '-----BEGIN RSA PRIVATE KEY-----\n$dataBase64\n-----END RSA PRIVATE KEY-----';
   }
 }
 
